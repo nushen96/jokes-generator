aed826b8ff8519a23fa5cd0a494e1999781e3304
Papi Diagne <abdiagne96@gmail.com>
2020-11-13 09:26:25 +0000

    added api fetch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import logo from "./logo.svg";
 import Joke from "./components/Joke";
+import React, { useState, useEffect } from "react";
 
 function App() {
   // Jokes API: http://www.icndb.com/api/
+  const apiURL = "http://api.icndb.com/jokes/random";
+  const [joke, setJoke] = useState({});
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    fetch(apiURL)
+      .then((data) => data.json())
+      .then((joke) => setJoke(joke));
+  }, []);
+
+  const updateCategory = (category) => {
+    setCategory(category);
+  };
+
+  const fetchJoke = (category) => {
+    fetch(category == "all" ? apiURL : apiURL + "?limitTo=[" + category + "]")
+      .then((data) => data.json())
+      .then((joke) => setJoke(joke));
+  };
+
+  console.log(joke);
+
   return (
     <div className="root-container">
       <h1 className="row main-title">Nushen's joke generator</h1>
       <div className="row actions-row">
-        <select name="pets" id="pet-select">
+        <select
+          name="pets"
+          id="pet-select"
+          onChange={(e) => updateCategory(e.target.value)}
+        >
           <option value="all" selected>
             All
           </option>
-          <option value="dog">Dog</option>
-          <option value="cat">Cat</option>
-          <option value="hamster">Hamster</option>
-          <option value="parrot">Parrot</option>
-          <option value="spider">Spider</option>
-          <option value="goldfish">Goldfish</option>
+          <option value="nerdy">Nerdy</option>
+          <option value="explicit">Explicit</option>
         </select>
-        <button>Make me laugh</button>
+        <button onClick={(e) => fetchJoke(category)}>Make me laugh! 😂</button>
       </div>
       <div className="row">
-        <Joke />
+        <Joke
+          jokeText={joke.value ? joke.value.joke : "No joke in that category"}
+        />
       </div>
     </div>
   );
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,15 +1,9 @@
 import React from "react";
 
-const Joke = () => {
+const Joke = ({ jokeText }) => {
   return (
     <div className="joke-container">
-      <p className="joke">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-        placerat facilisis blandit. Nam sit amet dolor lacus. Morbi rutrum
-        venenatis magna quis dignissim. Sed condimentum, justo sit amet lacinia
-        tincidunt, est nisi consequat ex, vitae ornare enim velit vitae mauris.
-        Suspendisse potenti.
-      </p>
+      <p className="joke" dangerouslySetInnerHTML={{__html: jokeText}}/>
     </div>
   );
 };
